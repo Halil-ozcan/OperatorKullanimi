bc147d512bf9beabaea4d3ed390b330450bc1520
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 15:56:38 +0300

    C# Operatorler Alistirma 1 Yapıldı.

--- OperatorlerAlistirma1/Program.cs
+++ OperatorlerAlistirma1/Program.cs
@@ -10,6 +10,29 @@ namespace OperatorlerAlistirma1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
+
+
+            Console.Write("İsiminizi Giriniz: ");
+            string isim = Console.ReadLine();
+
+            Console.Write("Soyisminizi Giriniz: ");
+            string soyisim = Console.ReadLine();
+
+            Console.Write("Yılınızı Giriniz: ");
+            string yilStr = Console.ReadLine();
+
+            Console.Clear();
+
+            int yil = Convert.ToInt32(yilStr);
+            int mevcutYil = 2024;
+            int yas = mevcutYil - yil;
+
+            string mesaj = "Merhaba " + isim + " " + soyisim + " " + yas + " Yasindasiniz";
+
+            Console.WriteLine(mesaj);
+
+            Console.ReadLine();
         }
     }
 }
