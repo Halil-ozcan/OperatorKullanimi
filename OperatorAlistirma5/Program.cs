5400d552c9f9ba2f3b3cbc608d8061881a91c892
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 18:40:41 +0300

    C# Operator Alistirma 5 Yapıldı.

--- OperatorAlistirma5/Program.cs
+++ OperatorAlistirma5/Program.cs
@@ -10,6 +10,22 @@ namespace OperatorAlistirma5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
+
+            Console.Write("Üçgen Çizimi için birinci değeri giriniz : ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Üçgen Çizimi için İkinci değeri giriniz : ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Üçgen Çizimi için Üçüncü değeri giriniz : ");
+            int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            bool donendeger = sayi1 < sayi2 + sayi3 && sayi2 < sayi1 + sayi3 && sayi3 < sayi1 + sayi2;
+
+            Console.WriteLine("sonuc : " + donendeger);
         }
     }
 }
