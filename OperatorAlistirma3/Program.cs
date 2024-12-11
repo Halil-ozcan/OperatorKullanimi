63ef578b175499db64b04cfbb36146334d357316
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 16:58:58 +0300

    C# Operator Alistirma 3 Yapıldı

--- OperatorAlistirma3/Program.cs
+++ OperatorAlistirma3/Program.cs
@@ -10,6 +10,22 @@ namespace OperatorAlistirma3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
+            Console.Write("Birinci sinav Notunu Giriniz : ");
+            int sinavNotu1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("İkinci sinav Notunu Giriniz : ");
+            int sinavNotu2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Üçüncü sinav Notunu Giriniz : ");
+            int sinavNotu3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            decimal sinavNotOrtalama = (sinavNotu1 + sinavNotu2 + sinavNotu3) / 3;
+
+            bool sonuc = sinavNotOrtalama >= 45;
+
+            Console.WriteLine("sonuc: " + " " + sonuc);
+            Console.ReadLine();
         }
     }
 }
