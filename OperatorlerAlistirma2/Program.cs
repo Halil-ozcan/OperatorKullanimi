5f7182fc0f2ca2ca34f4f45f3282588d40cf2114
Halil Özcan <halil_ozcann@outlook.com>
2024-12-11 16:17:36 +0300

    C# Operator Alistirma 2 Yapıldı.

--- OperatorlerAlistirma2/Program.cs
+++ OperatorlerAlistirma2/Program.cs
@@ -10,6 +10,26 @@ namespace OperatorlerAlistirma2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba Ekrana iki sayi değeri girer misiniz? ");
+
+            Console.Write("Birinci sayiyi Giriniz : ");
+            decimal sayi1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("İkinci sayiyiy Giriniz : ");
+            decimal sayi2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+
+            decimal toplama = sayi1 + sayi2;
+            decimal cikarma = sayi1 - sayi2;
+            decimal carpma = sayi1 * sayi2;
+            decimal bolme = sayi1 / sayi2;
+            decimal mod = sayi1 % sayi2;
+
+            Console.WriteLine("Toplama : " + toplama + " Çıkarma : " + cikarma + " Çarpma : " + carpma + " Bölme : " + bolme + " Mod : " + mod);
+            Console.ReadLine();
+
+
         }
     }
 }
